b1bde1482eecfabb3fa73cfa2e93d6265ba12115
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-19 13:23:05 -0700

    Wraped <App> in Router

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router } from "react-router-dom";
 
 import Home from './Home';
 import About from './About';
@@ -6,3 +5,4 @@ import Contact from './Contact';
 import Navigation from './Navigation';
 
 export { Home, About, Contact, Navigation };
+
